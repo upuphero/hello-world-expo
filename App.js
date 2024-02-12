b338bd06bd891c974b4d429f611f0e2db5089662
upuphero <540864988@qq.com>
2024-02-11 23:26:34 -0700

    build

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+
+import React from "react";
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
